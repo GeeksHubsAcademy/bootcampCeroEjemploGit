8c41d12cec102f4d2dc81a8c54e81468128567f9
Dave86dev <davidochando1986@gmail.com>
2021-09-30 18:16:31 +0200

    JavaScript implementado, contador con Prompt básico con éxito.

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+console.log("Hola buenas, esto es una prueba, hola mundo");
+
+let numero = parseInt(prompt("Querido usuario, dígame un número."));
+
+//console.log(numero);
+
+for(let i = 1; i <= numero; i++){
+
+    console.log(i);
+
+}
